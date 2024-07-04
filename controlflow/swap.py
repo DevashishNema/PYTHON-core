05d4a4fad6035c40152439c3a41b130094aba5c5
Devashish Nema <devnema3@gmail.com>
2024-07-04 14:19:32 +0530

    swap

---
+++ controlflow/swap.py
@@ -0,0 +1,41 @@
+# swaping with temp
+# a=int(input("enter 1 no. "))
+# b=int(input("enter 2 no. "))
+# print("before swap :",a,b)
+
+# x=a
+# a=b
+# b=x
+# print("after swap :",a,b)
+
+
+
+#by using multiplication and division
+# a=int(input("enter 1 no. "))
+# b=int(input("enter 2 no. "))
+# print("before : ",a,b)
+
+# a=a*b
+# b=a/b
+# a=a/b
+# print("after swap : ",a,b)
+
+
+# by using addition and substraction
+# a=int(input("enter 1 no. "))
+# b=int(input("enter 2 no. "))
+# print("before : ",a,b)
+
+# a=a+b
+# b=a-b
+# a=a-b
+# print("after swap : ",a,b)
+
+
+# without using any variable and addition or multiplication
+a=int(input("enter 1 no. "))
+b=int(input("enter 2 no. "))
+print("before : ",a,b)
+
+a,b=b,a
+print("after swap : ",a,b)
